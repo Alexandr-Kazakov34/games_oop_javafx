6f03d0f6f0ad62f6f220df857a0c2f44140a4da8
Alexandr <Rem271@mail.ru>
2024-03-01 18:35:50 +0300

    3. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -23,15 +23,15 @@ class BishopBlackTest {
         assertThat(copy.position().equals(newCell)).isEqualTo(true);
     }
 
-//    @Test
-//    public void testWay() {
-//        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
-//        Cell dest = Cell.F5;
-//        Cell[] actualSteps = bishopBlack.way(dest);
-//        Cell[] expectedSteps = {Cell.D7, Cell.E6, Cell.F5};
-//
-//        for (int i = 0; i < expectedSteps.length; i++) {
-//            assertThat(expectedSteps[i]).isEqualTo(actualSteps[i]);
-//        }
-//    }
+/*    @Test
+    public void testWay() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell dest = Cell.F5;
+        Cell[] actualSteps = bishopBlack.way(dest);
+        Cell[] expectedSteps = {Cell.D7, Cell.E6, Cell.F5};
+
+        for (int i = 0; i < expectedSteps.length; i++) {
+            assertThat(expectedSteps[i]).isEqualTo(actualSteps[i]);
+        }
+    }*/
 }
